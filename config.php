8ba2fb2d2a8c6042db4390b57c69cf5443296a80
Jeffrey Davidson <3826104+JeffreyDavidson@users.noreply.github.com>
2026-09-21 20:01:30 -0400

    feat: migrate marketing site to Jigsaw (#5)

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'production' => false,
+    'baseUrl' => '',
+    'title' => 'Ringside',
+    'description' => 'Wrestling promotion management for independent promoters.',
+    'build' => [
+        'destination' => 'public',
+    ],
+    'collections' => [],
+];
